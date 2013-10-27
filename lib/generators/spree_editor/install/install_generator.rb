261674cfa1c5b993c50e105b16f761799b0ff7fc
Brandon H. <tuxdapenguin@gmail.com>
2013-10-26 17:00:05 -0700

    :D

--- lib/generators/spree_editor/install/install_generator.rb
+++ lib/generators/spree_editor/install/install_generator.rb
@@ -4,8 +4,8 @@ module SpreeEditor
       source_root File.expand_path('../../../../templates', __FILE__)
 
       def add_javascripts
-        inject_into_file "app/assets/javascripts/admin/all.js", " *= require admin/spree_editor\n", before: /\*\//, verbose: true
-
+        append_file 'app/assets/javascripts/admin/all.js', " *= require admin/spree_editor\n"
+      end
       def add_stylesheets
         inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_editor\n", before: /\*\//, verbose: true
       end
