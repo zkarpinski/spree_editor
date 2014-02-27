612c50101bd36bdfdb52118fc218d469b50a5d11
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-02-27 10:21:13 -0500

    Remove devise.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'spree', github: 'spree/spree', branch: '2-2-stable'
-gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: '2-2-stable'
 
 gemspec
--- lib/spree_editor.rb
+++ lib/spree_editor.rb
@@ -1,6 +1,6 @@
 require 'ckeditor'
 require 'spree_core'
-require 'spree_editor/version'
 require 'spree_editor/engine'
+require 'spree_editor/version'
 require 'tinymce-rails'
 require 'coffee_script'
